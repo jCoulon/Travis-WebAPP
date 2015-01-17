4eae1382d1fb4bf57e8e6867fc51aeaf040aa31f
Jonathan Coulon <jonathancoulon@Jonathans-MacBook-Pro.local>
2015-01-17 03:25:35 -0500

    ctrl

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,21 +1,74 @@
 /**
  * Created by jo on 20/11/14.
  */
-angular.module("TravisAPP",['ngAnimate','ui.router'])
-.controller("GlobalController",GlobalController);
+angular.module("TravisAPP")
+
+
+    .factory("dataIndicateur",function(){
+
+        function dataIndicateur(name){
+            this.name = name;
+        }
+
+        dataIndicateur.prototype  = {
+
+            bind : function(indicateur,scope){
+                this.dataModel = indicateur.dataModel;
+            },
+            suppData : function(){
+
+            },
+            updateData : function(newData){
+                this.dataModel = newData;
+            }
+
+        };
+
+        return dataIndicateur;
+    })
+
+    .factory("dataPie",['$http','dataIndicateur',function(dataIndicateur,$http){
+
+        function dataPie(){
+            this.name = " data Pie instance";
+        };
+
+        dataPie.prototype = Object.create(dataIndicateur.prototype);
+
+        dataPie.prototype.loadData = function(){
+            //  $http.get();
+        };
+
+        return dataPie;
+    }])
+
+    .factory("indicateursGenerique",['dataPie',function(dataPie){
+        return [
+            {
+                name:"pie",
+                directive:"chartPie",
+                dataModel : dataPie
+            }];
+    }])
 
 
 
+    .controller("dashboardIndicateur",['indicateursGenerique',dashboardIndicateur]);
 
-function GlobalController(){
 
-    var ctrl = this;
+/**
+ * Gestion indicateurs
+ */
+function dashboardIndicateur(indicateursGenerique){
 
-    this.search = false; //On n'affiche pas l'input search par défaut
-    this.currentUser = null;
-    this.titre = "MON TITRE";
-    this.setCurrentUser = function(user){
-        ctrl.currentUser = user;
-    }
+    /**
+     * En attendant modif, recuperation des indicateurs enregistré pour l'utilisateur connecté
+     * @type {{titre: string}[]}
+     */
+    var indicateurs = [{titre : "radar"},{titre:"time"}];
 
+    this.dashboardOptions = {
+        indicateurs :indicateurs, // Indicateurs à charger
+        indicateurGenerique : indicateursGenerique //
+    };
 };
