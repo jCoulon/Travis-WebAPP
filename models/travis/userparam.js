18cb869e78f9ee18b5da5c042b49c40204fc458d
Jonathan Coulon <jonathancoulon@Jonathans-MacBook-Pro.local>
2015-02-18 22:40:17 -0500

    Rest create group

---
+++ models/travis/userparam.js
@@ -0,0 +1,38 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+    var Notes = sequelize.define("Userparam", {
+        IDParam: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true} ,
+        Username: DataTypes.STRING,
+        ParamName1	: DataTypes.STRING,
+        ParamValue1	:DataTypes.STRING,
+        Description1	:DataTypes.STRING
+    },{
+
+        // don't add the timestamp attributes (updatedAt, createdAt)
+        timestamps: false,
+
+        // don't delete database entries but set the newly added attribute deletedAt
+        // to the current date (when deletion was done). paranoid will only work if
+        // timestamps are enabled
+        paranoid: true,
+
+        // don't use camelcase for automatically added attributes but underscore style
+        // so updatedAt will be updated_at
+        underscored: true,
+
+        // disable the modification of tablenames; By default, sequelize will automatically
+        // transform all passed model names (first parameter of define) into plural.
+        // if you don't want that, set the following
+        freezeTableName: true,
+
+        // define the table's name
+        tableName: 'User_param',
+
+        id: 'IDParam'
+
+    });
+
+    return Notes;
+};
+
--- routes/api/users.js
+++ routes/api/users.js
@@ -6,13 +6,35 @@ var config = require("../../config/auth");
 
 router.get('/getUser/:username/:password', config.findById);
 
-
 router.post("/login", config.seConnecter);
-
 /**
  * Verifie si l'utilisateur est deja connecte ou non
  */
 router.get("/connected", config.utilisateurDejaConnecte);
+
+router.get("/group/:idGroup", function (req, res) {
+
+    models.Userparam.findOne({
+        where: {
+            ParamName1: req.params.group,
+            Username: req.params.username
+        }
+    }).then(function (data) {
+        res.json(data)
+    })
+});
+
+router.post("/group/", function (req, res) {
+    var value = req.body;
+    models.Userparam.create({
+        Username: value.username,
+        ParamName1: value.param,
+        ParamValue1: value.value,
+        Description1: value.desc
+    }).then(function () {
+        res.send("")
+    })
+});
 
 
 module.exports = router;
